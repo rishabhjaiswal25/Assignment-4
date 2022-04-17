c8a82d60103a569153eaf1e73e54ae25299b7af3
THIRUKUMARAN14 <103258849+THIRUKUMARAN14@users.noreply.github.com>
2022-04-17 12:09:25 +0530

    Create 01.java

---
+++ Thirukumaran/01.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Assign4_1 {
+	static int cal_increment(int sal,float rating) {
+		int inc=0;
+		if(rating >=1 && rating <= 4) {
+			inc=10;
+		}
+		else if(rating >= 4.1 && rating <=7) {
+			inc=25;
+		}
+		else {
+			inc=30;
+		}
+		int inc_sal=sal+(sal*inc/100);
+		return inc_sal;
+	}
+	public static void main(String args[]) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the salary: ");
+		int sal=s.nextInt();
+		System.out.println("Enter the Performance appraisal rating: ");
+		float rating= s.nextFloat();
+		if(rating<1 || rating>10 || sal<0) {
+			System.out.println("Invalid Input");
+		}else {
+			System.out.println(cal_increment(sal,rating));
+		}
+		
+	}
