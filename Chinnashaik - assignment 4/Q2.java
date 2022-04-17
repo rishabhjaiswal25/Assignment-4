c76473ccc2a38be2c41134dd221a2d81c3a50823
Chinna Saheb Shaik <103763723+Chinna-saheb@users.noreply.github.com>
2022-04-17 13:42:48 +0530

    Create Q2.java

---
+++ Chinnashaik - assignment 4/Q2.java
@@ -0,0 +1,35 @@
+package Assignment4;
+import java.util.Scanner;
+public class Assignment4question2 {
+
+	static void cal_high_place(int cs,int ec,int me) {
+		if(cs<0 || ec<0 || me<0) {
+			System.out.println("Input is invalid");
+		}
+		else if(cs==ec && ec==me) {
+			System.out.println("None of the department has got the highest placement");
+		}
+		else {
+			System.out.println("Highest placement");
+			int maxx=0;
+			if(cs>ec && cs>me) {maxx=cs;}
+			else if(ec>cs && ec>me) {maxx=ec;}
+			else {maxx=me;}
+			if(maxx==cs) {System.out.println("CS");}
+			if(maxx==ec) {System.out.println("EC");}
+			if(maxx==me) {System.out.println("ME");}
+		}	
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the no of students in CS:");
+		int cs=s.nextInt();
+		System.out.println("Enter the no od students in EC:");
+		int ec=s.nextInt();
+		System.out.println("Enter the no of students in ME:");
+		int me=s.nextInt();
+		cal_high_place(cs,ec,me);
+	}
+
+}
