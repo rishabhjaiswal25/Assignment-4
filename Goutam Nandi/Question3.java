929eb424ae81301ee90bb72369592cc260b29b79
goutam1999 <87134744+goutam1999@users.noreply.github.com>
2022-04-17 17:14:53 +0530

    Create Question3.java

---
+++ Goutam Nandi/Question3.java
@@ -0,0 +1,48 @@
+package assignment_4;
+import java.util.*;
+public class Question3
+{
+     static void number() {
+         Scanner sc=new Scanner(System.in);
+         System.out.println("Provide first number of range ");
+         
+         int a=sc.nextInt();
+         System.out.println("Provide last number of range ");
+         int b=sc.nextInt();
+         int flag;
+         if(a<=0 || b<=0 || a>=b)
+
+         System.out.println("Provide valid input");
+         else
+         {
+             Inner:
+             while(a<=b)
+             {
+                 if(a==2)
+                 System.out.print(a+" ");
+                 else if(a==1)
+                 {
+                     a++;
+                     continue;
+                 }
+                 else
+                 {
+                     flag=0;
+                     outer:
+                     for(int i=2;i<=a/2;i++)
+                     {
+                         if(a%i==0)
+                         {
+                             flag=1;
+                             break outer;
+                         }
+                     }
+                     if(flag==0)
+                     System.out.print(a+" ");
+                 }
+                 a++;
+             }
+         }
+     }public static void main(String[] args) {
+    number();}
+ }
