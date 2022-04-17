ffb739fb7cddd17af5a40cb3531d20f9ad911fcd
Sahauday <103258047+Sahauday@users.noreply.github.com>
2022-04-17 12:26:50 +0530

    Create 1.java

---
+++ Uday_Saha/1.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Main{
+
+     static void method() {
+
+        
+
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Enter the salary");
+
+        int salary = sc.nextInt();
+
+        System.out.println("Enter the Performance appraisal rating");
+
+        float rating = sc.nextFloat();
+
+        if(salary<1||rating<1.0||rating>5.0){
+
+            System.out.println("Invalid Input");
+
+            System.exit(0);
+
+        }
+
+        else if(rating>=1&&rating<=3){
+
+            salary=salary+(int)(0.1*salary);
+
+            System.out.println(salary);
+
+        }
+
+        else if(rating>3&&rating<=4){
+
+            salary=salary+(int)(0.25*salary);
+
+            System.out.println(salary);
+
+        }
+
+        else {
+
+            salary=salary+(int)(0.3*salary);
+
+            System.out.println(salary);
+
+        }
+
+    } public static void main(String[] args) {
+    method();}
+
+}
